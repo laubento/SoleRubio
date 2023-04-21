7f9977d2ca570a9938f398fbc54ea047304f1435
laubento <lauiribarrenn@gmail.com>
2023-04-21 16:36:29 -0300

    Highlights ready in desktop

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
-
+import { Highlights } from '@/components/Home/Highlights/Highlights'
+import style from '../styles/index.module.css' 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -9,6 +10,9 @@ export default function Home() {
       <Head>
         <title>Sole Rubio</title>
       </Head>
+      <div className={style["container-highlights"]}>
+        <Highlights />
+      </div>
     </main>
   )
 }
