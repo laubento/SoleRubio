74a2ca028f7b4d0c20ce6083af05fa32fb07c31a
Agagus <agus.zanetti4@gmail.com>
2023-04-24 19:17:07 -0300

    fix image names

--- src/components/Home/Slider/Slider.js
+++ src/components/Home/Slider/Slider.js
@@ -1,22 +1,39 @@
-import style from './Slider.module.css'
-import Image from 'next/image'
-import slides from '../../../images/Slides.jpg'
-import slides2 from '../../../images/Slides2.jpg'
-
+import style from "./Slider.module.css";
+import Image from "next/image";
+import slides from "../../../images/slides.jpg";
+import slides2 from "../../../images/slides2.jpg";
 
 export function Slider() {
-
-    return (
-        <div className={style["container-main"]}>
-            <div className={style['container-backgroud']}>
-                <Image src={slides} id={style['IMG']} alt='slides' className={style['image-backgroud-one']}/>
-                <Image src={slides2} id={style['IMG']} alt='slides' className={style['image-backgroud-two']}/>
-                <div className={style["container-slider"]}>
-                    <Image src={slides} id={style['IMG']} alt='slides' className={style['image-slide-one']}/>
-                    <Image src={slides2} id={style['IMG']} alt='slides' className={style['image-slide-two']}/>
-                </div>
-            </div>
-                
+  return (
+    <div className={style["container-main"]}>
+      <div className={style["container-backgroud"]}>
+        <Image
+          src={slides}
+          id={style["IMG"]}
+          alt="slides"
+          className={style["image-backgroud-one"]}
+        />
+        <Image
+          src={slides2}
+          id={style["IMG"]}
+          alt="slides"
+          className={style["image-backgroud-two"]}
+        />
+        <div className={style["container-slider"]}>
+          <Image
+            src={slides}
+            id={style["IMG"]}
+            alt="slides"
+            className={style["image-slide-one"]}
+          />
+          <Image
+            src={slides2}
+            id={style["IMG"]}
+            alt="slides"
+            className={style["image-slide-two"]}
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
