0f251380886e5696f5feab6a23e5d44137b71c35
laubento <lauiribarrenn@gmail.com>
2023-04-27 09:15:56 -0300

    testing

--- src/components/Campaigns/ImageCampaigns/ImageCampaings.js
+++ src/components/Campaigns/ImageCampaigns/ImageCampaings.js
@@ -1,12 +1,67 @@
 import style from './ImageCampaigns.module.css'
+import Image from 'next/image'
+import test from '../../../images/pepe.png'
+import Link from 'next/link'
+export function ImageCampaigns() {
 
-export function ImageCampaigns(){
-
-    return(
+    return (
         <div className={style["container-main"]}>
             <div className={style["container-campaigns"]}>
-                
+                <Link href={'/campaigns/name'}>
+                    <Image src={test} alt='test' className={style["img"]} />
+                    <p>Backtage</p>
+                </Link>
+                <Link href={'/campaigns/name'}>
+                    <Image src={test} alt='test' className={style["img"]} />
+                    <p>Backtage</p>
+                </Link>
+                <Link href={'/campaigns/name'}>
+                    <Image src={test} alt='test' className={style["img"]} />
+                    <p>Backtage</p>
+                </Link>
+                <Link href={'/campaigns/name'}>
+                    <Image src={test} alt='test' className={style["img"]} />
+                    <p>Backtage</p>
+                </Link>
+                <Link href={'/campaigns/name'}>
+                    <Image src={test} alt='test' className={style["img"]} />
+                    <p>Backtage</p>
+                </Link>
+
+                <Link href={'/campaigns/name'}>
+                    <Image src={test} alt='test' className={style["img"]} />
+                    <p>Backtage</p>
+                </Link>
+                <Link href={'/campaigns/name'}>
+                    <Image src={test} alt='test' className={style["img"]} />
+                    <p>Backtage</p>
+                </Link>
+                <Link href={'/campaigns/name'}>
+                    <Image src={test} alt='test' className={style["img"]} />
+                    <p>Backtage</p>
+                </Link>
+                <Link href={'/campaigns/name'}>
+                    <Image src={test} alt='test' className={style["img"]} />
+                    <p>Backtage</p>
+                </Link>
             </div>
         </div>
     )
 }
+
+// export function ImageCampaigns({name}){
+
+//     return(
+//         <div className={style["container-main"]}>
+                // <div className={style["container-campaigns"]}>
+                //     <ul>
+                //         {name.map((id) => {
+                //             return (
+                //                 <li key={id}>{id}</li>
+                //             )
+                //         })}
+                //     </ul>
+                // </div>
+//         </div>
+//     )
+// }
