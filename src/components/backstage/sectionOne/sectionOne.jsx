dfe5c2e39645eae202ec2869dd56edd4b1ca9d53
Noelia Ferrer <noelia.c.ferrer@gmail.com>
2023-05-02 12:33:43 -0300

    backstage

--- src/components/backstage/sectionOne/sectionOne.jsx
+++ src/components/backstage/sectionOne/sectionOne.jsx
@@ -11,22 +11,23 @@ const SectionOne = () => {
           alt=""
           className={styles.photo1}
         />
-        
-        <h2 className={styles.title1}>FOTOGRAFIA</h2>
+        <div className={styles.title1}>
+          <h2 style={{ position: "inherit", fontSize: '1.2rem' }}>FOTOGRAFIA</h2>
+        </div>
 
         <img
           src="https://i0.wp.com/fashionablymale.net/wp-content/uploads/2020/11/Mariano-Ontanon-by-Sole-Rubio-Dromo-Hombre8.jpg?resize=819%2C1024&ssl=1"
           alt=""
           className={styles.photo2}
         />
-        <div className={styles.title2}>
-        <h2 style={{position: 'inherit'}}>ADVERTISING</h2>
-        </div>
+          <h2 className={styles.title2}>ADVERTISING</h2>
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdwrfObKHZjRzWJNo8IKAqmocJjXGrQUvZX-yvWAI9w86pWPvWfYguZYZwu0QpK4XTib4&usqp=CAU"
           alt=""
           className={styles.photo3}
         />
+          <h2 className={styles.title3}>BOOKS</h2>
+
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1kQvXF7jbfxwx7pXi4FhgI1aRWzVZhsJhUi9ZvDYz_nZkHApysCIYbnOKp_coGdzzZxg&usqp=CAU"
           alt=""
@@ -37,19 +38,19 @@ const SectionOne = () => {
           alt=""
           className={styles.photo5}
         />
-         <img
+        <img
           src="https://i0.wp.com/fashionablymale.net/wp-content/uploads/2020/11/Mariano-Ontanon-by-Sole-Rubio-Dromo-Hombre8.jpg?resize=819%2C1024&ssl=1"
           alt=""
           className={styles.photo6}
-          />
-           <img
+        />
+        <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdwrfObKHZjRzWJNo8IKAqmocJjXGrQUvZX-yvWAI9w86pWPvWfYguZYZwu0QpK4XTib4&usqp=CAU"
           alt=""
           className={styles.photo7}
         />
-        <h2 className={styles.title4}>EDITORIAL</h2>
+          <h2 className={styles.title4}>EDITORIAL</h2>
 
-         <img
+        <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdwrfObKHZjRzWJNo8IKAqmocJjXGrQUvZX-yvWAI9w86pWPvWfYguZYZwu0QpK4XTib4&usqp=CAU"
           alt=""
           className={styles.photo8}
@@ -61,7 +62,7 @@ const SectionOne = () => {
         width={100}
         height={"auto"}
         className={styles.background}
-        alt= ''
+        alt=""
         // style={{ opacity: "0.5", margin: "3rem", position: 'fixed' }}
       />
     </div>
