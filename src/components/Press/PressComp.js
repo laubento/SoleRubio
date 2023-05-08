79d451d3632549213452cac903f87cebf754ec54
Agagus <agus.zanetti4@gmail.com>
2023-05-08 09:42:16 -0300

    press-cloudinary

--- src/components/Press/PressComp.js
+++ src/components/Press/PressComp.js
@@ -6,21 +6,81 @@ import Image from "next/image";
 
 import React from "react";
 
-const PressComp = () => {
+const PressComp = ({ data }) => {
   return (
     <div className={style["container-main"]}>
       <h3 className={style.titles}>PRENSA</h3>
       <div className={style["img-container"]}>
+        <Image
+          src={data.resources[0].url}
+          className={style.images}
+          alt="press"
+          width={1000}
+          height={600}
+        />
+        <Image
+          src={data.resources[1].url}
+          className={style.images}
+          alt="press"
+          width={1000}
+          height={1000}
+        />
+        <Image
+          src={data.resources[2].url}
+          className={style.images}
+          alt="press"
+          width={1000}
+          height={600}
+        />
+        <Image
+          src={data.resources[0].url}
+          className={style.images}
+          alt="press"
+          width={1000}
+          height={600}
+        />
+        <Image
+          src={data.resources[1].url}
+          className={style.images}
+          alt="press"
+          width={1000}
+          height={1000}
+        />
+        <Image
+          src={data.resources[2].url}
+          className={style.images}
+          alt="press"
+          width={1000}
+          height={600}
+        />
+        <Image
+          src={data.resources[0].url}
+          className={style.images}
+          alt="press"
+          width={1000}
+          height={600}
+        />
+        <Image
+          src={data.resources[1].url}
+          className={style.images}
+          alt="press"
+          width={1000}
+          height={1000}
+        />
+        <Image
+          src={data.resources[2].url}
+          className={style.images}
+          alt="press"
+          width={1000}
+          height={600}
+        />
         <Image src={img} className={style.images} alt="press" />
-        <Image src={img2} className={style.images} alt="press" />
+        {/* <Image src={img2} className={style.images} alt="press" />
         <Image src={img3} className={style.images} alt="press" />
         <Image src={img} className={style.images} alt="press" />
         <Image src={img2} className={style.images} alt="press" />
         <Image src={img3} className={style.images} alt="press" />
-        <Image src={img} className={style.images} alt="press" />
-        <Image src={img2} className={style.images} alt="press" />
-        <Image src={img3} className={style.images} alt="press" />
-        <Image src={img} className={style.images} alt="press" />
+        <Image src={img} className={style.images} alt="press" /> */}
       </div>
     </div>
   );
--- src/pages/press.js
+++ src/pages/press.js
@@ -1,10 +1,42 @@
 import PressComp from "../components/Press/PressComp";
 import style from "../styles/press.module.css";
+import dotenv from "dotenv";
 
-export default function Press() {
+export default function Press({ data }) {
+  console.log(data);
+
   return (
     <div className={style.containerPress}>
-      <PressComp />
+      <PressComp data={data} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  dotenv.config();
+  const name = process.env.CLOUD_NAME;
+  const key = process.env.CLOUD_KEY;
+  const secret = process.env.CLOUD_SECRET;
+  let info = {};
+
+  await fetch(
+    `https://api.cloudinary.com/v1_1/${name}/resources/image/upload?prefix=Prensa/&max_results=500`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Basic " + btoa(`${key}:${secret}`),
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return (info = data);
+    })
+    .catch((error) => console.error(error));
+
+  return {
+    props: {
+      data: info,
+    },
+  };
 }
