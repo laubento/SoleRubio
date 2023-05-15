e684eb253fa85882715641f2acfd3c96266d3b53
unknown <noelia.c.ferrer@gmail.com>
2023-05-15 09:24:32 -0300

    testing

--- src/components/backstage/sectionOne/sectionOne.jsx
+++ src/components/backstage/sectionOne/sectionOne.jsx
@@ -8,18 +8,25 @@ const SectionOne = ({ backImages }) => {
   return (
     <div className={styles.container}>
       <div className={styles.grid}>
-        <img src={backImages[0]} alt="image" className={styles.photo1} />
+        <div className={styles.photo1}>
+          <img src={backImages[0]} alt="image"
+          //className={styles.photo1} 
+          />
+        </div>
         <h2 className={styles.title1}>FOTOGRAFIA</h2>
-        <img src={backImages[1]} alt="image" className={styles.photo2} />
+   
+          <img src={backImages[0]} alt="image" className={styles.photo2} />
+
+      
         <h2 className={styles.title2}>ADVERTISING</h2>
-        <img src={backImages[2]} alt="image" className={styles.photo3} />
-        <img src={backImages[3]} alt="image" className={styles.photo4} />
+        <img src={backImages[0]} alt="image" className={styles.photo3} />
+        <img src={backImages[0]} alt="image" className={styles.photo4} />
         <h2 className={styles.title3}>BOOKS</h2>
-        <img src={backImages[4]} alt="image" className={styles.photo5} />
-        <img src={backImages[5]} alt="image" className={styles.photo6} />
+        <img src={backImages[0]} alt="image" className={styles.photo5} />
+        <img src={backImages[0]} alt="image" className={styles.photo6} />
         <h2 className={styles.title4}>EDITORIAL</h2>
-        <img src={backImages[6]} alt="image" className={styles.photo7} />
-        <img src={backImages[7]} alt="image" className={styles.photo8} />
+        <img src={backImages[0]} alt="image" className={styles.photo7} />
+        <img src={backImages[0]} alt="image" className={styles.photo8} />
 
         <Image
           src={backgroundImg}
@@ -27,7 +34,7 @@ const SectionOne = ({ backImages }) => {
           height={200}
           className={styles.background}
           alt=""
-          style={{ opacity: "0.3", margin: "3rem", position: "fixed" }}
+        //style={{ background: 'yellow', opacity: "0.3", margin: "1rem", position: "fixed" }}
         />
       </div>
       {/* <div className={styles.background}>
